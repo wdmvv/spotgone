d2467854f3ac102763e868ae185a035731b846f7
wdm <78714307+wdmvv@users.noreply.github.com>
2024-08-07 22:47:12 +0500

    almost done with GetPlaylist

--- spoton/spotify.go
+++ spoton/spotify.go
@@ -37,6 +37,46 @@ type Album struct {
 	} `json:"items"`
 }
 
+// TODO: https://developer.spotify.com/documentation/web-api/reference/get-playlists-tracks
+type Track struct {
+	Album struct {
+	} `json:"album"`
+}
+
+type Playlist struct {
+}
+
+// i have no idea how to do this efficiently so almost same structs /shrug
+// this one is for processing response while capital one is for storing tracks only
+// ill see how this turns out to be and maybe will tweak stuff, who knows
+type playlistResponse struct {
+	Total int `json:"total"`
+	Items []struct {
+		Track struct {
+			Album struct {
+				Images []struct {
+					URL    string `json:"url"`
+					Height int    `json:"height"`
+					Width  int    `json:"width"`
+				} `json:"images"`
+				Name    string `json:"name"`
+				Release string `json:"release_date"`
+				Artists []struct {
+					Name string `json:"name"`
+				} `json:"artists"`
+			} `json:"album"`
+			Artists []struct {
+				Name string `json:"name"`
+			} `json:"artists"`
+			Disc       int    `json:"disc_number"`
+			DurationMs int    `json:"duration_ms"`
+			Name       string `json:"name"`
+			Number     int    `json:"track_number"`
+			Popularity int    `json:"popularity"`
+		} `json:"track"`
+	} `json:"items"`
+}
+
 // this should be in some kind of config but eh
 var (
 	tokenEndpoint string = "https://accounts.spotify.com/api/token"
@@ -115,4 +155,32 @@ func (u *User) GetAlbum(id string) (Album, error) {
 	d = json.NewDecoder(resp.Body)
 	err = d.Decode(&a)
 	return a, err
+}
+
+func (u *User) GetPlaylist(id string) (Playlist, error) {
+	if u.AuthToken == "" {
+		return Playlist{}, &ErrNoAuth{}
+	}
+
+	// yes i couldve used a body for this but why bother? this is not a complex request, just simple math
+	// if this was bigger then i wouldve used struct -> json marshaling and proper body and not nil
+	url := fmt.Sprintf("https://api.spotify.com/v1/playlists/%s/tracks?offset=0&limit=50", id)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return Playlist{}, err
+	}
+	req.Header.Add("Authorization", "Bearer "+u.AuthToken)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return Playlist{}, err
+	} else if resp.StatusCode != http.StatusOK {
+		return Playlist{}, &ErrBadRequest{"playlist", resp.StatusCode}
+	}
+	d := json.NewDecoder(resp.Body)
+	var pl playlistResponse
+	err = d.Decode(&pl)
+	if err != nil {
+		return Playlist{}, err
+	}
+	return Playlist{}, nil
 }
